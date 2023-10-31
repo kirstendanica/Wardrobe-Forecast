fbcf3887773107bd826c29e4e02ddf2db268b0e0
kirsten d <kirdanic@gmail.com>
2023-10-31 00:58:44 -0400

    Implement weather API & custom image search API

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const db = require('./dbConfig');
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// OpenWeatherMap API
+const OPEN_WEATHER_MAP_API_KEY = '8c395d2b7bfeb6ab230779e70a3f6850';
+
+// Google Custom Search API
+const GOOGLE_CUSTOM_SEARCH_API_KEY = 'AIzaSyB1g6ZqJZo8b_NbFs-CWGfRlpazj6SwlkM';
+const GOOGLE_CUSTOM_SEARCH_ENGINE_ID = 'f59a6b8ea1e0f44c3';
 
 // Parse body of incoming POST requests
 app.use(express.json());
@@ -35,6 +43,42 @@ app.post('/login', (req, res) => {
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
+    });
+});
+
+app.get('/weather/:city', (req, res) => {
+  const city = req.params.city;
+
+  axios
+    .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_MAP_API_KEY}`)
+    .then(response => {
+      const weatherData = response.data;
+      res.json(weatherData);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+app.get('/outfit/:weather/:gender', (req, res) => {
+  const { weather, gender } = req.params;
+
+  axios
+    .get(`https://www.googleapis.com/customsearch/v1`, {
+      params: {
+        key: GOOGLE_CUSTOM_SEARCH_API_KEY,
+        cx: GOOGLE_CUSTOM_SEARCH_ENGINE_ID,
+        q: `${gender} outfit for ${weather} weather`,
+        searchType: 'image',
+        num: 5
+      }
+    })
+    .then(response => {
+      const outfitImages = response.data.items.map(item => item.link);
+      res.json(outfitImages);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
     });
 });
 
